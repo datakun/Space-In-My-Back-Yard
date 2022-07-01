52114ae74452c3959adb218df7e88cb80005b4e7
datakun <kimdatagoon@gmail.com>
2022-07-02 01:41:03 +0900

    /craw/yyyy-MM-dd 를 입력하면 해당 일의 apod 이미지를 다운 받는 기능 구현.

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,13 @@ import fs from 'fs';
 
 // functions
 
-const crawl = () => {
+const TODAY_URL = 'https://apod.nasa.gov/apod/astropix.html';
+const ARCHIVE_URL_PREFIX = 'https://apod.nasa.gov/apod/';
+const DOWNLOAD_PATH = './dist/apod/';
+
+const crawl = (url: string) => {
 	return new Promise<string>((resolve, reject) => {
-		request.get('https://apod.nasa.gov/apod/astropix.html', (error, response) => {
+		request.get(url, (error, response) => {
 			if (error) {
 				reject(error);
 			}
@@ -24,8 +28,9 @@ const extractImageUrl = (html: string): string => {
 	}
 
 	const $ = load(html);
-	const crawledRealtimeKeywords = $('body > center:nth-child(1) > p:nth-child(3) > a');
+	const crawledRealtimeKeywords = $('body > center > p > a > img');
 	const hrefList = $(crawledRealtimeKeywords)
+		.map((i, element) => $(element).parent('a'))
 		.filter((i, element) => $(element).attr('href') !== undefined)
 		.map((i, element) => $(element).attr('href'))
 		.get();
@@ -57,13 +62,32 @@ app.get('/', (request: Request, response: Response, next: NextFunction) => {
 });
 
 app.get('/crawl', async (request: Request, response: Response, next: NextFunction) => {
-	const result = await crawl();
 	try {
-		const prefix = 'https://apod.nasa.gov/apod/';
-		const imageUrl = prefix + extractImageUrl(result);
+		const result = await crawl(TODAY_URL);
+		const imageUrl = ARCHIVE_URL_PREFIX + extractImageUrl(result);
 		await downloadImage(imageUrl, './' + new Date().toISOString().slice(0, 10) + '.jpg');
 		response.send('ok');
 	} catch (error) {
+		console.error(error);
+		response.send(error);
+	}
+});
+
+app.get('/crawl/:date', async (request: Request, response: Response, next: NextFunction) => {
+	try {
+		const date = request.params.date;
+		// date 가 yyyy-MM-dd 형태인 지 검사
+		if (!/^\d{4}-\d{2}-\d{2}$/.test(date)) {
+			throw new Error('date is not yyyy-MM-dd');
+		}
+
+		// yyyy-MM-dd -> yyMMdd
+		const result = await crawl(ARCHIVE_URL_PREFIX + 'ap' + date.slice(2).replaceAll('-', '') + '.html');
+		const imageUrl = ARCHIVE_URL_PREFIX + extractImageUrl(result);
+		await downloadImage(imageUrl, DOWNLOAD_PATH + date + '.jpg');
+		response.send('ok');
+	} catch (error) {
+		console.error(error);
 		response.send(error);
 	}
 });
@@ -74,6 +98,11 @@ app.listen('8080', () => {
 Server started on: http://localhost:8080
 ########################################
 `);
+
+	// DOWNLOAD_PATH 경로가 존재하지 않으면 재귀적으로 폴더 만들기
+	if (!fs.existsSync(DOWNLOAD_PATH)) {
+		fs.mkdirSync(DOWNLOAD_PATH, { recursive: true });
+	}
 
 	const platform = process.platform;
 	if (platform === 'darwin') {
